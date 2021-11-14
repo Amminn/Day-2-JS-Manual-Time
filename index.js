f1e307d92ef9fa6aee18fffa9e5a7f5321fc760c
Amminn <55767303+Amminn@users.noreply.github.com>
2021-11-14 17:56:08 +0100

    js for another currrent clock

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // get the clock hands
-const hour = document.querySelector(".hour-hand");
-const min = document.querySelector(".min-hand");
-const second = document.querySelector(".second-hand"); 
-
+const hour = document.querySelectorAll(".hour-hand");
+const min = document.querySelectorAll(".min-hand");
+const second = document.querySelectorAll(".second-hand"); 
+console.log(min)
 // set the var of the clock 
 let hh = 0
 let mm = 0
@@ -11,24 +11,46 @@ let secondDeg = 90
 let minDeg = 90
 let hourDeg = 90
 
+// setting the current time 
+let d = new Date(); 
+d.getSeconds()
+d.getMinutes() 
+d.getHours()
+
+// setting the values of the current time
+const secCurentDeg =  (((d.getSeconds() * 6)) + 90)
+let secTime = secCurentDeg 
+const minCurentDeg =  (((d.getMinutes() * 6)) + 90)
+let minTime = minCurentDeg 
+const hourCurentDeg =  ((d.getHours() / 12) * 360 ) + 90
+let hourTime = minCurentDeg
+console.log(hourTime)
+
+
 // The hands start from deg 0, But the hands on the clock should start from 90deg
-second.style.transform += 'rotate('+secondDeg+'deg)'
-min.style.transform += 'rotate('+minDeg+'deg)'
-hour.style.transform += 'rotate('+hourDeg+'deg)'
+second[0].style.transform += 'rotate('+secondDeg+'deg)'
+second[1].style.transform += 'rotate('+secondDeg+'deg)'
+min[0].style.transform += 'rotate('+minDeg+'deg)'
+min[1].style.transform += 'rotate('+minDeg+'deg)'
+hour[0].style.transform += 'rotate('+hourDeg+'deg)'
+hour[1].style.transform += 'rotate('+hourDeg+'deg)'
+
+
 
 function timerS () {
     // each second the second hand sould rotat 6 deg
     secondDeg = secondDeg + 6
-    second.style.transform = 'rotate('+secondDeg+'deg)'
+    second[0].style.transform = 'rotate('+secondDeg+'deg)'
+    
     ss++
     if (ss == 60) {
         ss = 0
         mm++
         minDeg = minDeg + 6
         // each min the second hand sould rotat 6 deg
-        min.style.transform = 'rotate('+minDeg+'deg)'
+        min[0].style.transform = 'rotate('+minDeg+'deg)'
         hourDeg = hourDeg + 0.25
-        hour.style.transform = 'rotate('+hourDeg+'deg)'
+        hour[0].style.transform = 'rotate('+hourDeg+'deg)'
         if (mm == 60) {
             hh++
             mm = 0
@@ -40,6 +62,23 @@ function timerS () {
             }
         }
     }
+
+    // current time start here 
+
+    let c = new Date(); 
+    c.getSeconds()
+
+    secTime = secTime + 6
+    second[1].style.transform = 'rotate('+secTime+'deg)'
+    if (c.getSeconds() == 00) {
+        minTime = minTime + 6
+        min[1].style.transform = 'rotate('+minTime+'deg)'
+        hourTime = hourTime + 0.25
+        hour[1].style.transform = 'rotate('+hourTime+'deg)'
+    }
+    min[1].style.transform = 'rotate('+minTime+'deg)'
+    hour[1].style.transform = 'rotate('+hourTime+'deg)'
+
     setTimeout(timerS, 1000) // callback each 1s
 }
 
